18834eefb4bd705c90a1439626f7279bc685782d
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-08-22 01:27:46 +0300

    integration of the new kad plugins

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ module.exports = {
 
     init(config ={} ) {
 
-        KAD.plugins.PluginKademliaNodeMock.initialize();
-        KAD.plugins.PluginKademliaNodeHTTP.initialize();
-        KAD.plugins.PluginKademliaNodeWebSocket.initialize();
-
         global.PANDORA_PROTOCOL_OPTIONS = KAD.helpers.Utils.mergeDeep(Config, config);
 
     },
