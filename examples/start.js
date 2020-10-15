9da7ab870a4e6593fe2154ceb04e81b0623962e5
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-10-16 02:45:15 +0300

    remove research testnet config

--- examples/start.js
+++ examples/start.js
@@ -1,32 +1,35 @@
 const KAD = require('pandora-protocol-kad-reference')
 const PANDORA_PROTOCOL = require('./../index')
 const path = require('path');
+const config = require('../config/config')
 
 console.log("PANDORA PROTOCOL REFERENCE EXAMPLE");
 
-//const sybilKeys = KAD.helpers.ECCUtils.createPair();
-const sybilKeys = {
-    //WARNING! PRIVATE KEY IS PROVIDED ONLY FOR TESTING PURPOSES ONLY!
-    privateKey: Buffer.from("68a595199d55260b90d97e6714f27c2a22548f9ee4b2c61956eb628189a8e2ed", "hex"),
-    publicKey: Buffer.from("049cf62611922a64575439fd14e0a1190c40184c4d20a1c7179828693d574e84b94b70c3f3995b7a2cd826e1e8ef9eb8ccf90e578891ecfe10de6a4dc9371cd19a", 'hex'),
-    uri: 'http://pandoraprotocol.ddns.net:9090/challenge/', //LINK FOR THE PANDORA PROTOCOL SPARTACUS SYBIL PROTECT CHALLENGE
-    origin: 'http://pandoraprotocol.ddns.net:9090', //LINK FOR THE PANDORA PROTOCOL SPARTACUS SYBIL PROTECT ORIGIN
-}
+let sybilKeys = config.SYBIL_PROTECT_KEYS, sybilKeyIsNotEmpty = false;
+for (const sybilKey of sybilKeys)
+    if (!sybilKey.publicKey.equals(Buffer.alloc(65)) || ( sybilKey.privateKey && sybilKey.privateKey.equals(Buffer.alloc(32) )) ) {
+        sybilKeyIsNotEmpty = true;
+        break;
+    }
+
+if (!sybilKeyIsNotEmpty)
+    sybilKeys = [ KAD.helpers.ECCUtils.createPair() ];
+
 
 KAD.init({
     PLUGINS:{
         CONTACT_SYBIL_PROTECT: {
-            SYBIL_PUBLIC_KEYS: [ sybilKeys ],
+            SYBIL_PUBLIC_KEYS: sybilKeys,
         }
     }
 });
 
 PANDORA_PROTOCOL.init({});
 
-if (sybilKeys.privateKey)
-    console.info("SYBIL PRIVATE KEY", sybilKeys.privateKey.toString('hex') );
+if (sybilKeys[0].privateKey)
+    console.info("SYBIL PRIVATE KEY", sybilKeys[0].privateKey.toString('hex') );
 
-console.info("SYBIL PUBLIC KEY", sybilKeys.publicKey.toString('hex') );
+console.info("SYBIL PUBLIC KEY", sybilKeys[0].publicKey.toString('hex') );
 
 const port = Number.parseInt(process.env.PORT || 10000);
 
@@ -36,10 +39,11 @@ const node = new PANDORA_PROTOCOL.PandoraProtocolNode( path.resolve( __dirname +
 async function execute() {
 
     await node.start({ port });
-    console.info("BOOTSTRAP INFO:", KAD.library.bencode.encode( node.contact.toArray()).toString('hex') )
 
-    if (process.env.BOOTSTRAP){
-        const bootstrap = process.env.BOOTSTRAP;
+    console.info("BOOTSTRAP INFO:", KAD.library.bencode.encode( node.contact.toArray() ).toString('hex') )
+
+    if (process.env.BOOTSTRAP || config.BOOTSTRAP_CONTACT){
+        const bootstrap = process.env.BOOTSTRAP||config.BOOTSTRAP_CONTACT;
         console.log("bootstrap", bootstrap)
         const out = await node.bootstrap( bootstrap, true);
         console.log("BOOTSTRAPING...", out.length);
